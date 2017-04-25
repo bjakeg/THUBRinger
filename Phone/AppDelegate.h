c64a17d46abec5c1b0d138f8954d9e10f49a9769
bjakeg <bjakeg@gmail.com>
2017-04-24 19:40:47 -0500

    Initial Commit

---
+++ Phone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Phone
+//
+//  Created by Jake Graham on 4/24/17.
+//  Copyright © 2017 Jake Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Phone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Phone
+//
+//  Created by Jake Graham on 4/24/17.
+//  Copyright © 2017 Jake Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
